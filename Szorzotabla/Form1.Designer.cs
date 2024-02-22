e933337802e3ec35ded36c9a94d45a652c42248f
botondmajoross <botond.majoross@gmail.com>
2024-02-22 10:20:50 +0100

    szorzotabla

--- Szorzotabla/Form1.Designer.cs
+++ Szorzotabla/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace szoft_ABCDEF
+﻿namespace Szorzotabla
 {
     partial class Form1
     {
@@ -28,31 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(330, 154);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button button1;
     }
 }
